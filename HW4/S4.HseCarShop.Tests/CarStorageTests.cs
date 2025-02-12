3cac3436c793f038f3613b2499c7e41966131b89
seny03 <arseniysemkin3012@gmail.com>
2025-02-12 16:49:55 +0300

    HW4: CarStorageTests updated

--- HW4/S4.HseCarShop.Tests/CarStorageTests.cs
+++ HW4/S4.HseCarShop.Tests/CarStorageTests.cs
@@ -1,101 +1,93 @@
-﻿//using Bogus;
-//using S4.HseCarShop.Models;
-//using S4.HseCarShop.Services;
-//using S4.HseCarShop.Services.PedalCars;
+﻿using Bogus;
+using S4.HseCarShop.Models;
+using S4.HseCarShop.Models.HandCar;
+using S4.HseCarShop.Models.HybridCar;
+using S4.HseCarShop.Models.PedalCar;
+using S4.HseCarShop.Services;
+using S4.HseCarShop.Services.HandCars;
+using S4.HseCarShop.Services.HybridCars;
+using S4.HseCarShop.Services.PedalCars;
 
-//namespace S4.HseCarShop.Tests;
+namespace S4.HseCarShop.Tests;
 
-//public class CarStorageTests
-//{
-//    private readonly Faker _faker;
+public class CarStorageTests
+{
+    private readonly Faker _faker;
 
-//    public CarStorageTests()
-//    {
-//        _faker = new Faker();
-//    }
+    public CarStorageTests()
+    {
+        _faker = new Faker();
+    }
 
-//    [Fact]
-//    [Trait("TestCategory", "CarStorage")]
-//    public void GetCar_HaveCarsInStorage_ReturnsSuitableCar()
-//    {
-//        // Arrange
-//        var engineType = _faker.Random.Enum<CarType>();
+    [Fact]
+    [Trait("TestCategory", "CarStorage")]
+    public void GetCar_HaveCarsInStorage_ReturnsSuitableCar()
+    {
+        // Arrange
+        var engineType = _faker.Random.Enum<EngineType>(); // Use EngineType instead of CarType
 
-//        var carStorage = new CarStorage();
+        var carStorage = new CarStorage();
 
-//        var pedalCarFactory = new PedalCarFactory();
-//        var handCarFactory = new HandCarFactory();
+        var pedalCarFactory = new PedalCarFactory(new PedalEngineFactory());
+        var handCarFactory = new HandCarFactory(new HandEngineFactory());
+        var hybridCarFactory = new HybridCarFactory(new HybridEngineFactory());
 
-//        var pedalSize = (uint)_faker.Random.Int(1, 100);
-//        var pedalEngine = new PedalCarParams(pedalSize);
+        var pedalSize = (uint)_faker.Random.Int(1, 100);
+        var pedalEngine = new PedalCarParams(pedalSize);
 
-//        carStorage.AddCar(pedalCarFactory, pedalEngine);
+        carStorage.AddCar(pedalCarFactory, pedalEngine);
+        carStorage.AddCar(handCarFactory, new HandCarParams());
+        carStorage.AddCar(hybridCarFactory, new HybridCarParams());
 
-//        var emptyEngine = new HandEngineParams();
+        // Act
+        var car = carStorage.GetCar(engineType);
 
-//        carStorage.AddCar(handCarFactory, emptyEngine);
-
-//        // Act
-//        var car = carStorage.GetCar(engineType);
-
-//        // Assert
-//        Assert.NotNull(car);
-//        Assert.Equal(engineType, car.Engine.Type);
-
-//        //car.Should().NotBeNull();
-//        //car.Engine.Type.Should().Be(engineType);
-
-//        // Немного ужастей
-//        // persons.Should().ContainSingle(x => x.name == "Егор").Which.age.Should().BeGreaterThan(9);
-//        // age.Should().BeGreaterThan(9)
-//        // 10.May(2020).At(21, 20, 30)
-
-//        // А может давайте еще так хД
-//        // a = b; => Move().b.To().a;
-//    }
-
-//    [Theory]
-//    [InlineData(CarType.Hand)]
-//    [InlineData(CarType.Pedal)]
-//    [InlineData((CarType)42)]
-//    [Trait("TestCategory", "CarStorage")]
-//    public void GetCar_EmptyStorage_ReturnsNull(CarType engineType)
-//    {
-//        // Arrange
-
-//        // Act
-//        var carStorage = new CarStorage();
-//        var car = carStorage.GetCar(engineType);
-
-//        // Assert
-//        Assert.Null(car);
-
-//        //car.Should().BeNull();
-//    }
+        // Assert
+        Assert.NotNull(car);
+        Assert.Equal(engineType, car.Engine.Type);
+    }
 
 
-//    [Theory]
-//    [MemberData(nameof(GetCarTestData))]
-//    [Trait("TestCategory", "CarStorage")]
-//    public void GetCar_EmptyStorage_ReturnsNull2(CarType engineType)
-//    {
-//        // Arrange
+    [Theory]
+    [InlineData(CarType.Hand)]
+    [InlineData(CarType.Pedal)]
+    [InlineData((CarType)42)]
+    [Trait("TestCategory", "CarStorage")]
+    internal void GetCar_EmptyStorage_ReturnsNull(CarType engineType)
+    {
+        // Arrange
+        var carStorage = new CarStorage();
 
-//        // Act
-//        var carStorage = new CarStorage();
-//        var car = carStorage.GetCar(engineType);
 
-//        // Assert
-//        Assert.Null(car);
+        // Act
+        var car = carStorage.GetCar((EngineType)engineType);
 
-//        //car.Should().BeNull();
-//    }
+        // Assert
+        Assert.Null(car);
+    }
 
-//    //public static IEnumerable<object[]> GetCarTestData =>
-//    public static TheoryData<CarType> GetCarTestData =>
-//    [
-//        CarType.Pedal,
-//        CarType.Hand,
-//        (CarType)42,
-//    ];
-//}
+
+    [Theory]
+    [MemberData(nameof(GetCarTestData))]
+    [Trait("TestCategory", "CarStorage")]
+    internal void GetCar_EmptyStorage_ReturnsNull2(CarType engineType)
+    {
+        // Arrange
+        var carStorage = new CarStorage();
+
+
+        // Act
+        var car = carStorage.GetCar((EngineType)engineType);
+
+        // Assert
+        Assert.Null(car);
+    }
+
+    //public static IEnumerable<object[]> GetCarTestData =>
+    public static IEnumerable<object[]> GetCarTestData()
+    {
+        yield return new object[] { CarType.Pedal };
+        yield return new object[] { CarType.Hand };
+        yield return new object[] { (CarType)42 };
+    }
+}
